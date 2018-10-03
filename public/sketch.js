b4aae9576baccf801b73417d05e7a25a459f74da
Xiaoqian <ivesonchen@gmail.com>
2018-10-03 18:50:23 -0400

    io server

--- public/sketch.js
+++ public/sketch.js
@@ -6,7 +6,7 @@
 var socket;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
   background(0);
   // Start a socket connection to the server
   // Some day we would run this server somewhere else
